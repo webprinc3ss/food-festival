2cb7377d6f2029a71bc5ac80b4acfc8f980987d5
Mystic Kitty <djabranton@gmail.com>
2021-02-10 11:58:24 -0800

    Fixed code.

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,40 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const path = require('path');
 const webpack = require("webpack");
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
-const path = require("path");
-
-
-const config = {
+module.exports = {
     entry: {
-        app: './assets/js/script.js',
-        events: './assets/js/events.js',
-        schedule: './assets/js/schedule.js',
-        tickets: './assets/js/tickets.js'
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
     },
     output: {
-        filename: '[name].bundle.js',
-        path: __dirname + '/dist'
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
     },
     module: {
         rules: [
             {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name(file) {
-                            return '[path][name].[ext]'
-                        },
-                        publicPath: function (url) {
-                            return url.replace('../', '/assets/')
-                        },
+                test: /\.jpg$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name(file) {
+                                return '[path][name].[ext]';
+                            },
+                            publicPath: function (url) {
+                                return url.replace('../', '/assets/');
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
                     }
-                },
-                {
-                    loader: 'image-webpack-loader',
-                },
-                ],
-            },
-        ],
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
@@ -44,7 +42,7 @@ const config = {
             jQuery: "jquery"
         }),
         new BundleAnalyzerPlugin({
-            analyzerMode: "static"
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
         }),
         new WebpackPwaManifest({
             name: "Food Event",
@@ -62,7 +60,5 @@ const config = {
             }]
         })
     ],
-    mode: "development"
+    mode: 'development'
 };
-
-module.exports = config;
